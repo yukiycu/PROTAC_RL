b705b917e0428ea0c5bc42b9d493339de8d06fe6
murakami@mnode <i180634g@gmail.com>
2024-06-21 18:58:25 +0900

    init

--- preprocess.sh
+++ preprocess.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+#SBATCH -p gnode02
+#SBATCH -n 1
+#SBATCH -J PROTAC-RL
+#SBATCH -o log/stout.%J
+#SBATCH --gres=gpu:1
+
+module load slurm cuda/11.8
+start_time=`date "+%Y-%m-%d %H:%M:%S"`
 
 dataset_name=PROTAC
-random=random
+random=canonical
 
 python preprocess.py -train_src data/${dataset_name}/${random}/src-train \
                      -train_tgt data/${dataset_name}/${random}/tgt-train \
@@ -11,3 +19,8 @@ python preprocess.py -train_src data/${dataset_name}/${random}/src-train \
                      -save_data data/${dataset_name}/${random}/ \
                      -src_seq_length 3000 -tgt_seq_length 3000 \
                      -src_vocab_size 1000 -tgt_vocab_size 1000 -share_vocab
+
+end_time=`date "+%Y-%m-%d %H:%M:%S"`
+
+echo "start" $start_time
+echo "end" $end_time
